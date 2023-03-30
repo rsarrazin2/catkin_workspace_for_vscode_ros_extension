74eec89114531fcb91fd0738965a0d200f726301
Roland Sarrazin <rolands@vingroup.net>
2023-03-30 05:58:11 +0000

    ROS debugger works with 0.8.4, not with latest (0.9.1)

--- src/foo/src/foo_node.cpp
+++ src/foo/src/foo_node.cpp
@@ -1,4 +1,12 @@
-int main()
+#include <ros/ros.h>
+
+#include <iostream>
+
+int main(int argc, char **argv)
 {
+    ros::init(argc, argv, "foo_node");
+
+    std::cout << "Foo's main()" << std::endl;
+
     return 0;
 }
